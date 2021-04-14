2830e2a8012c75a9f8007c755011c8be3dd3de88
Gonzague <gonzague.bt@gmail.com>
2021-04-14 17:39:32 +0200

    Pbs résolus, fonction objectif fonctionnel, contraintes 17,18,19 faites (retourne un résuslat, véracité non vérifié)

--- modele-MTZ-EAF/InstanceReader.py
+++ modele-MTZ-EAF/InstanceReader.py
@@ -31,19 +31,20 @@ class Data():
         listDonor=[] # liste des donneurs
         allPatient=[] #liste de listes des patients de chaque donneur
         allDonors=np.zeros((int(id_max)+1,int(P)))
+        allDonorsInP=np.zeros((int(id_max)+1,int(P)))
         
         #parcous du fichier html et remplissage des tableaux
         for donor in root_node.findall('entry'):
             #### Get the value from the attribute 'donor_id'###
             id = donor.attrib['donor_id']
-            listDonor.append(int(id))
-             if(donor.find('altruistic')!=None):
-                    pair.append(int(id))
-                else : 
-                    altruist.append(int(id))
+            listDonor.append(int(id))            
             #print('id du donneur :',int(id))
             Index[int(id)]=count
             listpatients=[]
+            if(donor.find('altruistic')==None):
+                pair.append(int(id))
+            else : 
+                altruist.append(int(id))
             for exchange in donor.findall('matches/match'):
                 patient=exchange.find('recipient')
                 score=exchange.find('score')
@@ -53,26 +54,31 @@ class Data():
                 arc= (int(id),int(patient.text))
                 listpatients.append(int(patient.text))
                 listExchange.append(arc)
-                
                 ind = allDonors[int(patient.text)][0]+1
                 allDonors[int(patient.text)][int(ind)]=int(id)
                 allDonors[int(patient.text)][0]=allDonors[int(patient.text)][0]+1
-               
+                
+                if(donor.find('altruistic')==None):
+                    allDonorsInP[int(patient.text)][int(ind)]=int(id)
+                    allDonorsInP[int(patient.text)][0]=allDonors[int(patient.text)][0]+1
+                
             allPatient.append(listpatients)
             count=count+1
-            
+    
         #print("liste des échanges : ",listExchange)
         self.nb_pair=len(pair)
         self.nb_donor=len(listDonor)
+        self.id_max=int(id_max)
         self.cost=cost #un double tableau, le coût de l'arc u_ij se récupère en cost[i][j]
         self.N=altruist #une liste contenant tous les donneurs seuls (ensemble N)
         self.P=pair #une liste contenant toutes les paires (ensemble P)
-        self.V=listDonor #une liste contenant tous les donneurs (ensemble V)
+        self.V=listDonor #une liste conetenant tous les donneurs (ensemble V)
         self.A=listExchange #une liste contenant tous les échanges (ensemble a : tous les arcs du graphe)
         self.allPatients=allPatient #une listes de liste : la liste des patients comaptible pour chaque donneur
         self.index=Index #pour un donneur i, index[i] retourne l'indice de i dans la liste allPatient : 
             # par exemple, pour le donneur id=12, la liste des patients compatible s'obtient pas allPatient[int(index[12])]
         self.allDonors=allDonors #un double tableau : pour un patient i, allDonors[i] est un tableau avec tous les donneurs
             #compatible avec 1. ATTENTION : allDonors[i][0]= le nombre de donneurs compatibles
+        self.allDonorsInP=allDonorsInP # idem mais avec uniquement les donneurs en paires
         self.Instance=(P,num) #un tuple ou le premier élément contient le nombre de patient, et le 2ème le numéro de l'instance 
                                 #(entre 0 et 5)
